e219486e075776de1655bf6bc2310489670c97e4
Kostia <47629852+kosssst@users.noreply.github.com>
2022-11-08 23:38:06 +0200

    added output

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import random
 
 if __name__ == "__main__":
-    N = 10000000
+    N = 100000
     x_yes = []
     y_yes = []
     x_no = []
@@ -13,9 +13,11 @@ if __name__ == "__main__":
         if (x >= 0 and x <= 2 and y >= 0 and y <= 3) or (x > 2 and x <= 4 and y >= 1 and y <= 2):
             x_yes.append(x)
             y_yes.append(y)
+            print(f"({x}, {y}) - yes")
         else:
             x_no.append(x)
             y_no.append(y)
+            print(f"({x}, {y}) - no")
     res = (len(x_yes)/N)*16
     print(res)
     plt.scatter(x_yes, y_yes, color= "green", marker= ".", s=10)
